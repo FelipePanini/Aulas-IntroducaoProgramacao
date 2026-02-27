605d4b7802811d69323df738e306d4d54ab13bdb
FelipePanini <felipepanini08@gmail.com>
2026-02-26 22:01:51 -0300

    Add files via upload

---
+++ Aula 2/Raiz.py
@@ -0,0 +1,13 @@
+# Raiz Quadrada
+
+print("#################################################################")
+print("                        Raiz Quadrada                        ")
+
+import math
+
+a = input("Entre com o primeiro número e tecle enter ")
+
+a= int(a)
+
+print(math.sqrt (a))
+input()
